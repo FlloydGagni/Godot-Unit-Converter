7116c87cb81c74c4215dd26f17cec7a5a4a9d7eb
Flloyd Nathaniel Gagni <fnbg720@gmail.com>
2025-06-06 19:15:48 +0800

    Optimized algorithm for dynamic menu and subcategory menu as well as label display
    Signed-off-by: Flloyd Nathaniel Gagni <fnbg720@gmail.com>

--- scripts/display_container.gd
+++ scripts/display_container.gd
@@ -1,16 +1,24 @@
 extends MarginContainer
 
 @onready var category_button: MenuButton = $VBoxContainer/HBoxContainer/MenuButton
-@onready var value_a_button: MenuButton = $VBoxContainer/VBoxContainer2/MenuButton
-@onready var value_b_button: MenuButton = $VBoxContainer/VBoxContainer3/MenuButton
+@onready var value_a_button: MenuButton = $VBoxContainer/VBoxContainer2/HBoxContainer/MenuButton
+@onready var value_b_button: MenuButton = $VBoxContainer/VBoxContainer3/HBoxContainer/MenuButton
 
 @onready var category_label: Label = $VBoxContainer/HBoxContainer/Label
-@onready var value_a_label: Label = $VBoxContainer/VBoxContainer2/Label
-@onready var value_b_label: Label = $VBoxContainer/VBoxContainer3/Label
+@onready var value_a_value_label: Label = $VBoxContainer/VBoxContainer2/Label
+@onready var value_b_value_label: Label = $VBoxContainer/VBoxContainer3/Label
+@onready var value_a_subcategory_label: Label = $VBoxContainer/VBoxContainer2/HBoxContainer/Label
+@onready var value_b_subcategory_label: Label = $VBoxContainer/VBoxContainer3/HBoxContainer/Label
 
 var category_popup
 var value_a_popup
 var value_b_popup
+
+var category_list : Array[String] = [
+	"Volume", "Length", "Weight and Mass", "Temperature",
+	"Energy", "Area", "Speed", "Time", "Power", 
+	"Data", "Pressure", "Angle"
+]
 
 var volume_subcategory : Array[String] = [
 	"Milliliters", "Cubic centimeters", "Liters", "Cubic Meters",
@@ -70,7 +78,20 @@ var pressure_subcategory : Array[String] = [
 var angle_subcategory : Array[String] = [
 	"Degrees", "Radians", "Gradians"
 ]
-
+var subcategory_list : Dictionary = {
+	"Volume" : volume_subcategory,
+	"Length" : length_subcategory, 
+	"Weight and Mass" : weight_and_mass_subcategory, 
+	"Temperature" : temperature_subcategory,
+	"Energy" : energy_subcategory, 
+	"Area" : area_subcategory, 
+	"Speed" : speed_subcategory, 
+	"Time" : time_subcategory, 
+	"Power" : power_subcategory, 
+	"Data" : data_subcategory, 
+	"Pressure" : pressure_subcategory, 
+	"Angle" : angle_subcategory 
+}
 
 func _ready() -> void:
 	category_popup = category_button.get_popup()
@@ -78,101 +99,25 @@ func _ready() -> void:
 	value_b_popup = value_b_button.get_popup() 
 	
 	category_popup.id_pressed.connect(_on_category_changed)
+	value_a_popup.id_pressed.connect(_on_value_a_subcategory_changed)
+	value_b_popup.id_pressed.connect(_on_value_b_subcategory_changed)
 
 func _on_category_changed(id : int) -> void :
-	match  category_button.get_popup().get_item_text(id):
-		"Volume":
-			value_a_popup.clear()
-			value_b_popup.clear()
-			
-			for subcategory in volume_subcategory:
-				value_a_popup.add_item( subcategory )
-				value_b_popup.add_item( subcategory )
-			
-		"Length":
-			value_a_popup.clear()
-			value_b_popup.clear()
-			
-			for subcategory in length_subcategory:
-				value_a_popup.add_item( subcategory )
-				value_b_popup.add_item( subcategory )
-			
-		"Weight and Mass":
-			value_a_popup.clear()
-			value_b_popup.clear()
-			
-			for subcategory in weight_and_mass_subcategory:
-				value_a_popup.add_item( subcategory )
-				value_b_popup.add_item( subcategory )
-			
-		"Temperature":
-			value_a_popup.clear()
-			value_b_popup.clear()
-			
-			for subcategory in temperature_subcategory:
-				value_a_popup.add_item( subcategory )
-				value_b_popup.add_item( subcategory )
-			
-		"Energy":
-			value_a_popup.clear()
-			value_b_popup.clear()
-			
-			for subcategory in energy_subcategory:
-				value_a_popup.add_item( subcategory )
-				value_b_popup.add_item( subcategory )
-			
-		"Area":
-			value_a_popup.clear()
-			value_b_popup.clear()
-			
-			for subcategory in area_subcategory:
-				value_a_popup.add_item( subcategory )
-				value_b_popup.add_item( subcategory )
-			
-		"Speed":
-			value_a_popup.clear()
-			value_b_popup.clear()
-			
-			for subcategory in speed_subcategory:
-				value_a_popup.add_item( subcategory )
-				value_b_popup.add_item( subcategory )
-			
-		"Time":
-			value_a_popup.clear()
-			value_b_popup.clear()
-			
-			for subcategory in time_subcategory:
-				value_a_popup.add_item( subcategory )
-				value_b_popup.add_item( subcategory )
-			
-		"Power":
-			value_a_popup.clear()
-			value_b_popup.clear()
-			
-			for subcategory in power_subcategory:
-				value_a_popup.add_item( subcategory )
-				value_b_popup.add_item( subcategory )
-			
-		"Data":
-			value_a_popup.clear()
-			value_b_popup.clear()
-			
-			for subcategory in data_subcategory:
-				value_a_popup.add_item( subcategory )
-				value_b_popup.add_item( subcategory )
-			
-		"Pressure":
-			value_a_popup.clear()
-			value_b_popup.clear()
-			
-			for subcategory in pressure_subcategory:
-				value_a_popup.add_item( subcategory )
-				value_b_popup.add_item( subcategory )
-			
-		"Angle":
-			value_a_popup.clear()
-			value_b_popup.clear()
-			
-			for subcategory in angle_subcategory:
-				value_a_popup.add_item( subcategory )
-				value_b_popup.add_item( subcategory )
+	var category_selected = category_button.get_popup().get_item_text(id)
+	
+	category_label.text = category_selected
+	
+	value_a_popup.clear()
+	value_b_popup.clear()
+	value_a_subcategory_label.text = "Choose Unit"
+	value_b_subcategory_label.text = "Choose Unit"
+	
+	for subcategory in subcategory_list.get(category_selected):
+		value_a_popup.add_item(subcategory)
+		value_b_popup.add_item(subcategory)
+
+func _on_value_a_subcategory_changed(id : int) -> void :
+	value_a_subcategory_label.text = value_a_button.get_popup().get_item_text(id)
+
+func _on_value_b_subcategory_changed(id : int) -> void :
+	value_b_subcategory_label.text = value_b_button.get_popup().get_item_text(id)
